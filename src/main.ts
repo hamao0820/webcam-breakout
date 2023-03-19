927af3bab07965be329c4cf7134db497073ad255
浜田　俊祐 <hamashun0820@icloud.com>
2023-03-19 11:52:15 +0900

    modelをプロパティにしました

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ class Main {
     private readonly webcam: Webcam;
     private readonly controllerDataset: ControllerDataset;
     private ui: Ui;
+    private model?: tf.LayersModel;
+
     constructor() {
         this.webcam = new Webcam();
         this.controllerDataset = new ControllerDataset(2);
@@ -22,19 +24,25 @@ class Main {
         this.ui.init(this.train.bind(this), this.controllerDataset, Model.truncatedMobileNet);
     }
 
-    private async  train(units: number) {
+    private async build(units: number) {
+        this.model = Model.build(units);
+    }
+
+    private async train(units: number) {
+        this.build(units);
+        if (!this.model) throw Error("先にビルドをしてください。`model.build(units: number)`");
+
         if (!this.controllerDataset.xs || !this.controllerDataset.ys)
             throw Error("訓練データが存在しません。先にデータを追加してください。");
-        const model = Model.build(units);
 
         const optimizer = tf.train.adam(this.ui.getLeaningRate());
-        model.compile({ optimizer: optimizer, loss: "categoricalCrossentropy" });
+        this.model.compile({ optimizer: optimizer, loss: "categoricalCrossentropy" });
         const batchSize = Math.floor(this.controllerDataset.dataSize * this.ui.getBatchSizeFraction());
         if (!(batchSize > 0)) {
             throw new Error(`Batch size is 0 or NaN. Please choose a non-zero fraction.`);
         }
         console.log("学習開始");
-        await model.fit(this.controllerDataset.xs, this.controllerDataset.ys, {
+        await this.model.fit(this.controllerDataset.xs, this.controllerDataset.ys, {
             batchSize,
             epochs: this.ui.getEpochs(),
             callbacks: {
